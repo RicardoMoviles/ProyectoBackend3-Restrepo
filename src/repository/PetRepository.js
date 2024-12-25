9792cc315477489fcd3a71508c6c3e1108858e9d
RicardoRpo <ricar2011.medallo@gmail.com>
2024-12-25 00:12:43 -0500

    Endpoint /generateData creado y funcionando, se verifica con metodos GET de users y pets

--- src/repository/PetRepository.js
+++ src/repository/PetRepository.js
@@ -1,9 +1,14 @@
-const GenericRepository =require("./GenericRepository.js");
+const GenericRepository = require("./GenericRepository.js");
 
 class PetRepository extends GenericRepository {
     constructor(dao) {
         super(dao);
     }
+
+    // Método para insertar varias mascotas
+    insertPets = (pets) => {
+        return this.create(pets); // Usa el método `create` de GenericRepository para insertar las mascotas
+    };
 }
 
 module.exports = PetRepository;
